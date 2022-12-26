1bb01dc5975f4038030b7e6774c5e6fa5dab7ad3
RaffDevs <rveronezdeveloper@gmail.com>
2022-12-26 16:32:20 -0300

    first commit

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
+enum GameType: String {
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameTypeLabel: UILabel!
+    
+    @IBOutlet weak var gameTypeSegmentControl: UISegmentedControl!
+    
+    @IBOutlet var balls: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showNumbers(for: .megasena)
     }
 
-
+    @IBAction func generateGame() {
+        switch gameTypeSegmentControl.selectedSegmentIndex {
+        case 0:
+            showNumbers(for: .megasena)
+        default:
+            showNumbers(for: .quina)
+        }
+    }
+    
+    func showNumbers(for type: GameType) {
+        gameTypeLabel.text = type.rawValue
+        
+        var game: [Int] = []
+        switch type {
+            case .megasena:
+                game = factoryNumbers(total: 6, universe: 60)
+                balls.last?.isHidden = false
+            case .quina:
+                game = factoryNumbers(total: 5, universe: 80)
+                balls.last?.isHidden = true
+        }
+        
+        for (index, game) in game.enumerated() {
+            balls[index].setTitle("\(game)", for: .normal)
+        }
+        
+    }
+    
+    func factoryNumbers(total: Int, universe: Int) -> [Int] {
+        var result: [Int] = []
+        
+        while result.count < total {
+            let randomNumber = Int.random(in: 0...universe)
+            
+            if !result.contains(randomNumber) {
+                result.append(randomNumber)
+            }
+        }
+        
+        return result.sorted()
+    }
+    
+    
 }
 
